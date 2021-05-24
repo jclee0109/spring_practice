953b2273b31f7256c3e24e218434cc4261f47e3a
jclee0109 <jclee0109@gist.ac.kr>
2021-05-24 16:00:37 +0900

    by section 5 + 6&7

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,2 +1,20 @@
-package hello.hellospring.controller;public class MemberController {
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private MemberService memberService;
+
+//    @Autowired //setter주입
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
+    @Autowired//결국 생성자 주입이 가장 좋다.. 동적으로 변경되는 경우가 거의 없으니까
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }//지금은 어떤 데이터베이스를 쓸 지 몰라서 Memory를 쓰고 있지만, 나중에 결정되면 한번에 쫙 바꿔주면 됨.
 }
